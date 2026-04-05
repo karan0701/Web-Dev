8d6b5ef75d02ec0ac14c17db9b83f4a07b6c8c8c
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-04-05 16:38:38 +0530

    .

---
+++ JS/lec55.js
@@ -0,0 +1,12 @@
+console.log("Hey, karan this side! ");
+var a = 10;
+var b = 20;
+// var 55a = 30; // not allowed
+console.log(a+b);
+console.log(typeof a, typeof b);
+
+
+
+// const a1 = 6;
+// a1  = a1+ 3;
+// not allowed because a1 is constant and cannot be reassigned., we can use "let" instead of "const" if we want to reassign the value of a1.    
